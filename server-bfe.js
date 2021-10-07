11d31c3f0af4e576b59f43e87c8bb8761eb26200
Anne Washington <amgaynor1@gmail.com>
2021-10-06 20:47:18 -0400

    Update server-bfe.js

--- server-bfe.js
+++ server-bfe.js
@@ -1,12 +1,12 @@
 //   Minimal BIBFRAME Editor Node.js server. To run from the command-line:
 //   node server-bfe.js
 
+var port = process.env.PORT || 8000;
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8000;
 
 app.use(express.static(__dirname + '/'));
 app.listen(port);
 
-console.log('BIBFRAME Editor running');
+console.log('BIBFRAME Editor running on ' + port);
 console.log('Press Ctrl + C to stop.'); 
